e3500314db10254bec2116d6446ff4b1683124e8
Ehsan Sam <ehsan_sm@ymail.com>
2021-01-17 20:52:56 +0330

    finding best solution

--- ga.py
+++ ga.py
@@ -19,11 +19,21 @@ def run(problem, params):
     empty_individual.position = None
     empty_individual.cost = None
 
+    # Best solution
+
+    bestsol = empty_individual.deepcopy()   # have no effect in other when changed
+    bestsol.cost = np.inf
+
     # Initialize population
     pop = empty_individual.repeat(npop)
     for i in range(0, npop):
         pop[i].position = np.random.uniform(varmin,varmax, nvar)
         pop[i].cost = costfunc(pop[i].position)
+        if pop[i].cost < bestsol:  # the better solution is the one with less cost
+            bestsol = pop[i].deepcopy()
+
+    # Best cost of itteration
+    bestcost = np.empty(maxit)
 
     # Output
     out = structure()
